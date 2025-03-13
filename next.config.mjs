52dcf08437acaa79ae7af418902460c3778e6a8c
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2025-03-12 18:09:10 -0600

    Add PWA support and update metadata in layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
