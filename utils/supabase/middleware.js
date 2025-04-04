a59b0cb1bdbab7e84c4b5486a553e61c5945f2a5
ARodrigues <alissonrodrigues.st@gmail.com>
2025-04-04 13:41:41 -0600

    Update middleware.js to allow access to public assets without authentication and include '/manifest.json' in the list of allowed pages.

--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -45,8 +45,20 @@ export async function updateSession(request) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  const allowedPages = ["/"];
+  const allowedPages = ["/", "/manifest.json"];
   const adminOnlyPages = allowAdminOnly;
+
+  // Allow access to public assets without authentication
+  if (
+    request.nextUrl.pathname.startsWith("/_next") ||
+    request.nextUrl.pathname.startsWith("/static") ||
+    request.nextUrl.pathname.startsWith("/images") ||
+    request.nextUrl.pathname.endsWith(".png") ||
+    request.nextUrl.pathname.endsWith(".svg") ||
+    request.nextUrl.pathname.endsWith(".ico")
+  ) {
+    return supabaseResponse;
+  }
 
   if (
     !user &&
